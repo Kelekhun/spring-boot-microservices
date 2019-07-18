ecf2bce657aafb8ff57f5992332fe1b3ef15c357
kelekhun <khadkadeepak@gmail.com>
2019-07-18 15:31:44 -0400

    Code clean!!!

--- ratings-data-service/src/main/java/com/yasinbanu/ratingsdataservice/models/UserRating.java
+++ ratings-data-service/src/main/java/com/yasinbanu/ratingsdataservice/models/UserRating.java
@@ -28,7 +28,11 @@ public class UserRating {
         this.setUserId(userId);
         this.setRatings(Arrays.asList(
                 new Rating("100", 3),
-                new Rating("200", 4)
+                new Rating("200", 4),
+                new Rating("300", 4),
+                new Rating("400", 3),
+                new Rating("500", 5)
+
         ));
     }
 }
